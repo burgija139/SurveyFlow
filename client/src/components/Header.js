b3fb0f8eb6916def8e8d25ed777092d9e99462af
dusan <dusan@git.com>
2025-03-26 15:56:55 +0100

    adding images

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,7 +23,7 @@ class Header extends React.Component {
             <nav>
                 <div className="nav-wrapper">
                     <Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo">
-                        Emaily!
+                        SurveyFlow!
                     </Link>
                     <ul id="nav-mobile" className="right">
                         {this.renderContent()}
